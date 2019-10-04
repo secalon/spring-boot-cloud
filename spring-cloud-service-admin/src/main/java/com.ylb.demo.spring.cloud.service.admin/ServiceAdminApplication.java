be1987e0c4ab153545f282f56db8ce03069b5701
secalon <1370417254@qq.com>
2019-10-04 22:29:23 +0800

    test

--- spring-cloud-service-admin/src/main/java/com.ylb.demo.spring.cloud.service.admin/ServiceAdminApplication.java
+++ spring-cloud-service-admin/src/main/java/com.ylb.demo.spring.cloud.service.admin/ServiceAdminApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ServiceAdminApplication {
-    public static void main(String [] args){
-        SpringApplication.run(ServiceAdminApplication.class,args);
+    public static void main(String [] arg){
+        SpringApplication.run(ServiceAdminApplication.class,arg);
     }
 }
